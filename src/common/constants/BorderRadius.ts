aeae04b842955fe4e354b422d2306a12ef5793db
long.tran <kdragonqng@gmail.com>
2024-11-25 15:41:59 +0700

    Add base (style,color,font,...)

---
+++ src/common/constants/BorderRadius.ts
@@ -0,0 +1,12 @@
+export const BorderRadius = {
+    br1000: 1000,
+    br100: 100,
+    br75: 75,
+    br24: 24,
+    br16: 16,
+    br12: 12,
+    br10: 10,
+    br8: 8,
+    br5: 5,
+    br4: 4,
+};
